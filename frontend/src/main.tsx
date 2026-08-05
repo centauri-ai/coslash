425e6452ec009c4a7472de63bec8fe9566941e65
calvintvu <calvin.vu@centauri-ai.tech>
2026-08-05 13:34:14 -0700

    feat: add dark mode toggle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { CoslashPage } from '@/pages/coslash/CoslashPage';
+import './index.css';
+import { getTheme, setTheme } from './lib/theme.ts';
+
+setTheme(getTheme(localStorage.getItem('theme')));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
